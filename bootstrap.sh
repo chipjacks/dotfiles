05c37b283129114c752a1a396e633c1799913080
Chip Jackson <chipj@ziprecruiter.com>
2021-12-20 11:03:00 -0800

    Add dry run to list files before syncing.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,16 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
+function dryRun() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "git-completion.bash" \
+		--exclude "git-completion.zsh" \
+		-avh --no-perms --dry-run . ~;
+}
+
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
@@ -20,6 +30,7 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
+	dryRun;
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
